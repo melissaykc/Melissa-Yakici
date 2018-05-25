1103041a9b656b352e1afd2b9966cc110e4d5375
melissa.ykc <melissa.ykc@hotmail.com>
2018-05-25 17:28:49 -0400

    medewerkers toegevoeg

--- Exonet/home.php
+++ Exonet/home.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" type="text/css" href="styles/home-grid.css">
     <link rel="stylesheet" type="text/css" href="styles/header.css">
     <link rel="stylesheet" type="text/css" href="styles/werk.css">
-    <link rel="stylesheet" type="text/css" href="styles/ontstaan.css">
+    <link rel="stylesheet" type="text/css" href="styles/waarom.css">
+    <link rel="stylesheet" type="text/css" href="styles/contact-home.css">
     
 		<title>
 			Exonet Belgium
@@ -18,52 +19,136 @@
       
       <div id="menu">
         <img src="images/logo.jpg">
-        <a href ="jobs.php" class="active"><b>Jobs</b></a>
-        <a href ="contact.php" class="active"><b>Contact</b></a>
-        <a href ="wat.php" class="active"><b>Wat wij doen</b></a>
-        <a href ="home.php" class="active"><b>Home</b></a>
+        <a href ="jobs.php" class="active">Jobs</a>
+        <a href ="contact.php" class="active">Contact</a>
+        <a href ="wat.php" class="active">Wat wij doen</a>
+        <a href ="home.php" class="active">Home</a>
       </div>
      
       <div id="header">
         <img id="head" src="images/head.jpg">
-        <p>
-          <b>Exonet Belgium</b>
-        </p>
+        <p><b>Exonet</b> Belgium</p>
       </div>
       
       <div id="werk">
         <div id="txt">
-          <p>
-            <b>Ons werk</b>
-          </p>
+          <p>Ons werk</p>
           <hr>
         </div>
         
+        <div id="foto1">
+          <img src="images/img2.jpg">
+          <div class="top-left">Verhuizingen</div>
+        </div>
         
-        <img id="foto1" src="images/img12.jpg">
-        <img id="foto2" src="images/img14.jpg">
-        <img id="foto3" src="images/img3.jpg">
-        <img id="foto4" src="images/img4.jpg">
-        <img id="foto5" src="images/img16.jpg">
-        <img id="foto6" src="images/img6.jpg">
-        <img id="foto7" src="images/img7.jpg">
-        <img id="foto8" src="images/img15.jpg">
+        <div id="foto2">
+          <img src="images/img1.jpg">
+          <div class="top-left">Dakgoten reinigen</div>
+        </div>
         
+        <div id="foto3">
+          <img src="images/img3.jpg">
+          <div class="top-left">Vloerreiniging</div>
+        </div>
+        
+        <div id="foto4">
+          <img src="images/img4.jpg">
+          <div class="top-left">Nieuwbouw schoonmaak</div>
+        </div>
+        
+        <div id="foto5">
+          <img src="images/img8.jpg">
+          <div class="top-left">Computerreiniging</div>
+        </div>
+        
+        <div id="foto6">
+          <img src="images/img6.jpg">
+          <div class="top-left">Kantoor schoonmaak</div>
+        </div>
+        
+        <div id="foto7">
+          <img src="images/img7.jpg">
+          <div class="top-left">Gazononderhoud</div>
+        </div>
+        
+        <div id="foto8">
+          <img src="images/img5.jpg">
+          <div class="top-left">Glaswas</div>
+        </div>
       </div>
       
-      <div id="ontstaan">
+      <div id="waarom">
         <div id="txt">
-          <p>
-            <b>Ontstaan</b>
-          </p>
+          <p>Waarom Exonet Belgium?</p>
           <hr>
         </div>
-        <div id="medewerker1">
-          
+        <div id="txt-waarom">
+          <p>
+            Exonet Belgium heeft op termijn een zeer stabiel klantenbestand opgebouwd, met heel wat klanten die besloten hebben ons als partner te omarmen. Voor onze directe manier van communiceren en onze flexibiliteit worden we geprezen.
+          </p>
+          <p>
+            Als externe partner zorgen we er eveneens voor dat uw sociale balans onder controle blijft. Alle risico’s en kosten zoals arbeidsongevallen, ziekte, lonen, verzekeringen e.d. nemen we van u over. Dit maakt een beduidend verschil. Reken hier nog planning en opvolging bij, en u weet wat Exonet Belgium voor u kan betekenen. 
+          </p>
+          <p>
+            Ook in het evalueren van uw personeel kunnen we u van dienst zijn. Dit kan gaan van externe inspecties, een basis aanreiken dat kan dienst doen tijdens evaluatiegesprekken, tot het volledig aansturen als externe partner.
+          </p>
+          <h3>
+            Onze medewerkers staan klaar om u aan te bieden wat u zoekt.
+          </h3>
+        </div>
+        <div id="medewerker1" class="medewerker">
+          <img src="images/werkn1.jpg" class="werk-img">
+          <h1>Jane Doe</h1>
+          <h2>Medewerker</h2>
+          <p>Wij zien het als onze plicht om u als klant zo goed mogelijk te adviseren en grondig te begeleiden. Spreek ons aan als u een speciale wens hebt.</p>
+          <div id="img-button1" class="werk-knop">
+            <a href="#">
+              Contact
+            </a>
+          </div>
         </div>
         
-        <div id="medewerker1">
-          
+        <div id="medewerker2" class="medewerker">
+          <img src="images/werkn2.jpg" class="werk-img">
+          <h1>John Doe</h1>
+          <h2>Medewerker</h2>
+          <p>Wij zien het als onze plicht om u als klant zo goed mogelijk te adviseren en grondig te begeleiden. Spreek ons aan als u een speciale wens hebt.</p>
+          <div id="img-button1" class="werk-knop">
+            <a href="contact.php">
+              Contact
+            </a>
+          </div>
+        </div>
+        
+        <div id="medewerker3" class="medewerker">
+          <img src="images/werkn3.jpg" class="werk-img">
+          <h1>Daniella Star</h1>
+          <h2>Medewerker</h2>
+          <p>Wij zien het als onze plicht om u als klant zo goed mogelijk te adviseren en grondig te begeleiden. Spreek ons aan als u een speciale wens hebt. </p>
+          <div id="img-button1" class="werk-knop">
+            <a href="#">
+              Contact
+            </a>
+          </div>
+        </div>
+        
+        <div id="medewerker4" class="medewerker">
+          <img src="images/werkn4.jpg" class="werk-img">
+          <h1>Mike Ross</h1>
+          <h2>Medewerker</h2>
+          <p>Wij zien het als onze plicht om u als klant zo goed mogelijk te adviseren en grondig te begeleiden. Spreek ons aan als u een speciale wens hebt.</p>
+          <div id="img-button1" class="werk-knop">
+            <a href="#">
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+      
+      <div id="contact-home">
+        <div id="txt">
+          <p>Contact</p>
+          <hr>
         </div>
       </div>
       
